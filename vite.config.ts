72b41ba6db910ddf3c367304383d3cf9df194e46
Nathan Cuevas <nathanacuevas97@gmail.com>
2025-01-15 13:49:04 -0800

    making sure my development and production are setup seperatly

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
+console.log(process.env.NODE_ENV);
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio/',
-  plugins: [react(), TanStackRouterVite()],
+  base: process.env.NODE_ENV === 'development' ? '/' : '/Portfolio/',
+  plugins: [react(), TanStackRouterVite({ apiBase: '/Portfolio/' })],
 });

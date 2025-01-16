84a0cad24370b4cd4e9155c5da2ada2283592973
Nathan Cuevas <nathanacuevas97@gmail.com>
2025-01-15 16:31:21 -0800

    Setup all my icons that I will need for now and my basic navigation

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,23 +2,32 @@ import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 export const Route = createRootRoute({
-  
   component: () => {
-    console.log(process.env.NODE_ENV);
     return (
-      <>
-        <div className="p-2 flex gap-2">
-          <Link to="/" className="[&.active]:font-bold">
-            Home
-          </Link>
-          <Link to="/about" className="[&.active]:font-bold">
-            About
-          </Link>
-        </div>
-        <hr />
-        <Outlet />
+      <div>
+        <header>
+          <div className="flex items-center justify-between max-w-screen-xl mx-auto px-8">
+            <Link to="/">Nathan Cuevas</Link>
+            <div className="p-2 flex gap-6">
+              <Link to="/my-work" className="[&.active]:font-bold">
+                My Work
+              </Link>
+              <Link to="/projects" className="[&.active]:font-bold">
+                Projects
+              </Link>
+              <Link to="/contact-me" className="[&.active]:font-bold">
+                Contact Me
+              </Link>
+            </div>
+            <p>English</p>
+          </div>
+          <hr />
+        </header>
+        <main className="max-w-screen-sm mx-auto px-8 py-12">
+          <Outlet />
+        </main>
         {process.env.NODE_ENV === 'development' ? <TanStackRouterDevtools /> : <> </>}
-      </>
+      </div>
     );
   },
 });

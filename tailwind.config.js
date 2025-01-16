44f424ee2581f4d570f319bbc21bfb1376b0c6cb
Nathan Cuevas <nathanacuevas97@gmail.com>
2025-01-15 16:28:22 -0800

    added styling for dark and light and some custom colors and an animation for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      background: 'hsl(var(--background))',
+      foreground: 'hsl(var(--foreground))',
+      accent: 'hsl(var(--accent))',
+      border: 'hsl(var(--border))',
+    },
+    extend: {
+      animation: {
+        marquee: 'marquee var(--duration) linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(calc(-100% - var(--gap)))' },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};

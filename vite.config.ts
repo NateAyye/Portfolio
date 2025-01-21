b32986046eefeabdcd07301995740b808c362ca4
Nathan Cuevas <nathanacuevas97@gmail.com>
2025-01-21 14:25:05 -0800

    trying to add custom 404 page for github

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react-swc';
+import copy from 'rollup-plugin-copy';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Portfolio/',
   plugins: [react(), TanStackRouterVite({ apiBase: '/Portfolio/' })],
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/configuration-options/
+      plugins: [copy({ targets: [{ src: '404.html', dest: 'dist' }] })],
+    },
+  },
 });
